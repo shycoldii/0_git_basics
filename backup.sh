5bd10e9e82da50cfef05679c63c0abed367dba22
Dasha Alexandrova <alexandrova-2001@mail.ru>
2021-02-12 12:04:05 +0300

    Add backup.sh

---
+++ backup.sh
@@ -0,0 +1,13 @@
+name="/etc/hosts"
+n=0
+while read line; do
+  IP=$(echo $line | awk '{ print $1 }')
+  re="^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
+  if [[ $IP =~ $re ]]; then
+        if ! [[ $IP == "127.0.0.1" ]];then
+                echo "$n $line"
+                n=$((n+1))
+        fi
+  fi
+done < $name
+
